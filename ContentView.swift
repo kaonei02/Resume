cc224537ad3968c3a75598bc2ff72a03ef19a752
kaonei02 <103878305+kaonei02@users.noreply.github.com>
2023-02-17 17:18:07 +0800

    Create ContentView.swift

---
+++ ContentView.swift
@@ -0,0 +1,167 @@
+//
+//  ContentView.swift
+//  Resume
+//
+//  Created by Yu shi Jian on 2023/2/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var isShowingContact = false
+    let me = Resume.shared
+    
+    var body: some View {
+        ScrollView {
+            VStack (spacing: 30) {
+                cardView
+                
+                Text(me.bio).font(.title3).lineSpacing(
+                    8)
+                
+                Button {
+                    isShowingContact = true
+                } label: {
+                    Text("Contact Me")
+                        .foregroundColor(.white)
+                        .font(.title2.weight(.bold))
+                        .padding(.vertical, 10)
+                        .frame(maxWidth: .infinity)
+                        .background(RoundedRectangle(cornerRadius: 20).foregroundColor(.brown))
+                }
+                
+                Text("Skill")
+                    .bold().font(.title2)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                
+                skillView
+                
+                Text("Experience")
+                    .bold().font(.title2)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                
+                experienceView
+          
+                Text("Education")
+                    .bold().font(.title2)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                
+                educationView
+                
+                
+                
+                
+                
+                
+            }
+            .padding()
+            .overlay(
+                Color.black.opacity(isShowingContact ? 0.5 : 0)
+                    .onTapGesture { isShowingContact = false }
+            )
+            .overlay(alignment: .top) { contactView }
+            .animation(.spring(), value: isShowingContact)
+        }.background(Color(uiColor: .secondarySystemBackground))
+    }
+    
+    // MARK: - subViews
+    
+    var cardView: some View {
+        HStack {
+            Image("avatar")
+                .resizable().aspectRatio(contentMode: .fit)
+                .clipShape(Circle())
+                .frame(width: 170)
+            
+            VStack (spacing: 20) {
+                Text(me.name)
+                    .font(.title).bold()
+                Text(me.title)
+                Label(me.location, systemImage: "location.fill").foregroundColor(.secondary)
+            }
+        }
+    }
+    
+    
+    var skillView: some View {
+        HStack {
+            ForEach(me.skills, id: \.self) { skill in
+                VStack {
+                    Image(skill.lowercased())
+                        .resizable().aspectRatio(contentMode: .fit)
+                        .frame(width: 50)
+                    Text(skill)
+                }.frame(width: 80)
+            }
+        }
+    }
+    
+    
+    var experienceView: some View {
+        VStack (spacing: 0)  {
+            ForEach(me.experiences.indices, id: \.self) { index in
+                HStack {
+                    DotLineShape(lineWidth: 2,
+                                 dotSize: 10,
+                                 style: index == 0 ? .bottomHalf :
+                                        index == me.experiences.count - 1 ? .topHalf : .full)
+                    .frame(width: 70)
+                    .foregroundColor(.secondary)
+                    
+                    let exp = me.experiences[index]
+                    VStack (spacing: 8) {
+                        Group {
+                            Text("\(exp.start) － \(exp.end)").foregroundColor(.secondary)
+                            Text(exp.title).bold()
+                            Text(exp.company).padding(.bottom, 30)
+                        }.frame(maxWidth: .infinity, alignment: .leading)
+                    }
+                }
+            }
+        }
+    }
+    
+    
+    var educationView: some View {
+        VStack (spacing: 0)  {
+            ForEach(me.educations.indices, id: \.self) { index in
+                HStack {
+                    DotLineShape(lineWidth: 2,
+                                 dotSize: 10,
+                                 style: index == 0 ? .bottomHalf :
+                                        index == me.experiences.count - 1 ? .topHalf : .full)
+                    .frame(width: 70)
+                    .foregroundColor(.secondary)
+                    
+                    let exp = me.educations[index]
+                    VStack (spacing: 8) {
+                        Group {
+                            Text("\(exp.start) － \(exp.end)").foregroundColor(.secondary)
+                            Text(exp.title).bold()
+                            Text(exp.company).padding(.bottom, 30)
+                        }.frame(maxWidth: .infinity, alignment: .leading)
+                    }
+                }
+            }
+        }
+    }
+    
+    
+    var contactView: some View {
+        Group {
+            if (isShowingContact) {
+                ContactView(isShowing: $isShowingContact)
+                    .offset(y: UIScreen.main.bounds.maxY * 0.82)
+                    .transition(.slide)
+            }
+        }
+    }
+}
+
+
+
+struct Previews_ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
