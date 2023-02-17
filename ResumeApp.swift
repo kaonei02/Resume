96897f46b823d486fd26c96f076d1999db11aaf4
kaonei02 <103878305+kaonei02@users.noreply.github.com>
2023-02-17 17:18:10 +0800

    Create ResumeApp.swift

---
+++ ResumeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResumeApp.swift
+//  Resume
+//
+//  Created by Yu shi Jian on 2023/2/17.
+//
+
+import SwiftUI
+
+@main
+struct ResumeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
